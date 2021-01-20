c0039f41b4b87b3c53217cb8f592fbff3cbfdc7d
khabib97 <kawser.habib.dev@gmail.com>
2021-01-20 18:15:57 +0600

    typo remove

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ async function app(fastify, options){
     //old standard routing
     //fastify.register(require('./routes/baisc-router'))
 
-    //auto load base on directory 
+    //auto-load, based on directory 
     fastify.register(autoload,{
          dir: path.join(__dirname, 'routes')
     })
